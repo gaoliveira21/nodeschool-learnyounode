ec379c161e6abadf0921dae7366606b747509d6f
Gabriel Jose de Oliveira <gabriel.oliveira@liveonsolutions.com>
2020-05-16 09:36:20 -0300

    http collect completed

---
+++ http-collect/program.js
@@ -0,0 +1,11 @@
+const http = require('http');
+const bl = require('bl');
+
+const url = process.argv[2];
+
+http.get(url, (response) => {
+  response.pipe(bl((err, data) => {
+    console.log(data.length);
+    console.log(data.toString('utf8'));
+  }));
+});
